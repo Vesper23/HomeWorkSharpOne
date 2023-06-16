7d99cf0b2fa87c3a0f499789b28f831def092c0d
Valentin Likholat <phantom.231220@gmail.com>
2023-06-16 03:30:36 +0300

    Completed second task

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
+/*
 Console.Write("Введите первое число: ");
 string number = Console.ReadLine()!;
 int num1 = int.Parse(number);
@@ -26,9 +27,40 @@ else if (num1 < num2)
 else
 {
     Console.WriteLine("Числа равны.");
-}
+} 
+*/
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.Write("Введите первое число: ");
+string number = Console.ReadLine()!;
+int num1 = int.Parse(number);
+
+Console.Write("Введите второе число: ");
+number = Console.ReadLine()!;
+int num2 = int.Parse(number);
+
+Console.Write("Введите третье число: ");
+number = Console.ReadLine()!;
+int num3 = int.Parse(number);
+
+int max = 0;
+if ((num1 > num2) && (num1 > num3))
+{
+    max = num1;
+    Console.WriteLine($"Наибольшее число - {max}.");
+}
+else if ((num2 > num1) && (num2 > num3))
+{
+    max = num2;
+    Console.WriteLine($"Наибольшее число - {max}.");
+}
+else
+{
+    max = num3;
+    Console.WriteLine($"Наибольшее число - {max}.");
+}
 
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
